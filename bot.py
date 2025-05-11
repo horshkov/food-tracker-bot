a81ee2d0d0fa666d6c926a1edb8a9bbb4759936b
horshkov <165885719+horshkov@users.noreply.github.com>
2025-05-11 15:07:06 +0100

    Set logging to DEBUG for detailed logs

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ load_dotenv()
 # Enable logging
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+    level=logging.DEBUG
 )
 logger = logging.getLogger(__name__)
 
